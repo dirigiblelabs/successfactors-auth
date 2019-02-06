b8d49d1939b21549319f158f843c43533ada6163
ThuF <jordan.p.pavlov@gmail.com>
2019-02-06 15:44:37 +0200

    Initial Commit

---
+++ successfactors-auth/utils/config.js
@@ -0,0 +1,19 @@
+exports.getHost = function() {
+	// TODO Return the SuccessFactors Host
+	return null;
+};
+
+exports.getCompany = function() {
+	// TODO Return the SuccessFactors Company
+	return null
+};
+
+exports.getUser = function() {
+	// TODO Return the SuccessFactors User
+	return null;
+};
+
+exports.getPassword = function() {
+	// TODO Return the SuccessFactors Password
+	return null;
+};
